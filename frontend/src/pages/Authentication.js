b4bee04849acd30d7976083216942a440da3b4ab
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-11-15 13:49:04 -0700

    managing token expiration

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -36,11 +36,14 @@ export async function action({ request }) {
 	if (!response.ok) {
 		throw json({ message: "COULD NOT AUTHENTICATE USER!!" }, { status: 500 });
 	}
-	
-const resData = await response.json()
-const token = resData.token;
 
-localStorage.setItem("token", token)
+	const resData = await response.json();
+	const token = resData.token;
+
+	localStorage.setItem("token", token);
+	const expiration = new Date();
+	expiration.setHours(expiration.getHours() + 1);
+	localStorage.setItem("expiration", expiration.toISOString);
 
 	return redirect("/");
 }
