3b9a61af9c5d330c2b1f48be5fe548d5bf0dcde1
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-11-15 12:41:13 -0700

    added user logout

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -36,6 +36,11 @@ export async function action({ request }) {
 	if (!response.ok) {
 		throw json({ message: "COULD NOT AUTHENTICATE USER!!" }, { status: 500 });
 	}
-	//soon: manage that token
+	
+const resData = await response.json()
+const token = resData.token;
+
+localStorage.setItem("token", token)
+
 	return redirect("/");
 }
