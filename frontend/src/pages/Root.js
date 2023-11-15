1da7a9e4bbaa8817eac1a48666053c95869868ef
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-11-15 13:29:10 -0700

    added automatic logout after 1 hr

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,19 +1,32 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { useEffect } from "react";
 
-import MainNavigation from '../components/MainNavigation';
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
+
+import MainNavigation from "../components/MainNavigation";
 
 function RootLayout() {
-  // const navigation = useNavigation();
+	const token = useLoaderData();
+	const submit = useSubmit();
+	// const navigation = useNavigation();
+	useEffect(() => {
+		if (!token) {
+			return;
+		}
 
-  return (
-    <>
-      <MainNavigation />
-      <main>
-        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
-        <Outlet />
-      </main>
-    </>
-  );
+		setTimeout(() => {
+			submit(null, { action: "/logout", method: "post" });
+		}, 1 * 60 * 60 * 1000);
+	}, [token, submit]);
+
+	return (
+		<>
+			<MainNavigation />
+			<main>
+				{/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+				<Outlet />
+			</main>
+		</>
+	);
 }
 
 export default RootLayout;
